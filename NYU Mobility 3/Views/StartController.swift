f89348f75c6ef93c383e7b3e273cc93a19a911d9
Jin Kim <jinsungkim1210@gmail.com>
2021-02-05 20:19:46 -0500

    feat: redirects working

--- NYU Mobility 3/Views/StartController.swift
+++ NYU Mobility 3/Views/StartController.swift
@@ -10,9 +10,9 @@ import UIKit
 class StartController: UIViewController {
     
     // Redirects to the different versions
-    @IBOutlet weak var option1Button: BubbleButton!
-    @IBOutlet weak var option2Button: BubbleButton!
-    @IBOutlet weak var option3Button: BubbleButton!
+    @IBOutlet weak var option1Button: BubbleButton! // Video (Steps)
+    @IBOutlet weak var option2Button: BubbleButton! // Video (Steps + GPS)
+    @IBOutlet weak var option3Button: BubbleButton! // No Video
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +21,73 @@ class StartController: UIViewController {
 
     // Registers button clicks + validates + redirects
     @IBAction func button1Pressed(_ sender: Any) {
-    
+        if (validateEmail()) {
+            performSegue(withIdentifier: "Tracker1", sender: self)
+        }
     }
     
     @IBAction func button2Pressed(_ sender: Any) {
-        
+        if (validateEmail()) {
+            performSegue(withIdentifier: "Tracker2", sender: self)
+        }
     }
     
     @IBAction func button3Pressed(_ sender: Any) {
-        
+        if (validateEmail()) {
+            performSegue(withIdentifier: "Tracker3", sender: self)
+        }
+    }
+    
+    /**
+        Ensures that there is a valid email that exists so that files can be labeled properly
+     */
+    func validateEmail() -> Bool {
+        if (UserDefaults.standard.string(forKey: "email")! == "") {
+            showInputDialog(title: "Enter your email",
+                            subtitle: "Your email is needed to label files (No emails will be sent to you or from you)",
+                            actionTitle: "Add",
+                            cancelTitle: "Cancel",
+                            inputPlaceholder: "Ex: test@gmail.com",
+                            inputKeyboardType: .emailAddress, actionHandler:
+                                { (input: String?) in
+                                    self.save("email", input ?? "")
+                                })
+        } else { // if email is found -> tracking can be started
+            return true
+        }
+        return false
+    }
+    
+    func save(_ key: String, _ value: String) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: "\(key)")
+    }
+}
+
+extension UIViewController {
+    func showInputDialog(title: String? = nil,
+                         subtitle: String? = nil,
+                         actionTitle: String? = "Add",
+                         cancelTitle: String? = "Cancel",
+                         inputPlaceholder: String? = nil,
+                         inputKeyboardType: UIKeyboardType = UIKeyboardType.default,
+                         cancelHandler: ((UIAlertAction) -> Swift.Void)? = nil,
+                         actionHandler: ((_ text: String?) -> Void)? = nil) {
+
+        let alert = UIAlertController(title: title, message: subtitle, preferredStyle: .alert)
+        alert.addTextField { (textField:UITextField) in
+            textField.placeholder = inputPlaceholder
+            textField.keyboardType = inputKeyboardType
+        }
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { (action:UIAlertAction) in
+            guard let textField =  alert.textFields?.first else {
+                actionHandler?(nil)
+                return
+            }
+            actionHandler?(textField.text)
+        }))
+        alert.addAction(UIAlertAction(title: cancelTitle, style: .cancel, handler: cancelHandler))
+
+        self.present(alert, animated: true, completion: nil)
     }
 }
