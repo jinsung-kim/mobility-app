dbe5bd0320f3a50e4f024b89299ee90aa688c21f
Jin Kim <jinsungkim1210@gmail.com>
2021-04-08 18:14:22 -0400

    feat: debugging labels added to see calculated values and in session status

--- NYU Mobility 3/Views/Trackers/DebugController.swift
+++ NYU Mobility 3/Views/Trackers/DebugController.swift
@@ -10,12 +10,24 @@
 
 import UIKit
 import CoreLocation
+import CoreMotion
 
 class DebugController: UIViewController, CLLocationManagerDelegate {
     
     var state: Int = 0 // Treat as flag for state (0 - not tracking, 1 - tracking)
     
-    private var locationManager: CLLocationManager = CLLocationManager()
+    private let locationManager: CLLocationManager = CLLocationManager()
+    private let pedometer: CMPedometer = CMPedometer()
+    
+    // Default values are negative so we know how to update the values
+    var startTheta: Double = -1.0
+    var endTheta: Double = -1.0
+    
+    var distance: Int = 0
+    
+    // Labels for debugging process
+    @IBOutlet weak var sessionStatusLabel: UILabel!
+    @IBOutlet weak var veeringLabel: UILabel!
     
     // Buttons to start and stop sessions
     @IBOutlet weak var trackButton: UIButton!
@@ -36,11 +48,11 @@ class DebugController: UIViewController, CLLocationManagerDelegate {
             sender.setTitle("Stop", for: .normal)
             self.state = 1
         case 1:
-            locationManager.stopUpdatingHeading()
-            locationManager.stopUpdatingLocation()
-            sender.setTitle("Share", for: .normal)
+            stopTracking()
+            sender.setTitle("Restart", for: .normal)
             self.state = 2
         case 2:
+            sessionStatusLabel.text = "Session completed"
             sender.setTitle("Start", for: .normal)
             self.state = 0
         default: // Should never happen
@@ -48,16 +60,60 @@ class DebugController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    private func setup() {
+    func stopTracking() {
+        locationManager.stopUpdatingHeading()
+        locationManager.stopUpdatingLocation()
+        
+//        print("-------Results-------")
+//        print("Starting Theta: \(startTheta)")
+//        print("Ending Theta: \(endTheta)")
+//        print("Distance Traveled: \(distance)")
+        
+        let dTheta = abs(startTheta - endTheta)
+        let estVeer = abs(cos(dTheta) * Double(distance))
+        
+//        print("Estimated Veering: \(estVeer)")
+        
+        veeringLabel.text = "Estimated Veering: \(estVeer) m, dTheta: \(dTheta.truncate(places: 2))°"
+    }
+    
+    func startCountingSteps() {
+        pedometer.startUpdates(from: Date()) {
+          [weak self] pedometerData, error in
+          guard let pedometerData = pedometerData, error == nil else { return }
+            // Runs concurrently
+            DispatchQueue.main.async {
+                self?.distance = Int(truncating: pedometerData.distance ?? 0)
+            }
+        }
+    }
+    
+    func setup() {
         self.locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.headingAvailable() {
             self.locationManager.startUpdatingLocation()
             self.locationManager.startUpdatingHeading()
         }
+        
+        sessionStatusLabel.text = "Session in progress"
+        
+        startCountingSteps()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-        print(newHeading.magneticHeading)
+        // Consider averaging the last 5 values of this - to ensure that there are no outliers?
+        let curr = newHeading.magneticHeading
+        
+        if (startTheta == -1.0) {
+            startTheta = curr
+        }
+        endTheta = curr // Whatever the last value of curr is -> Is where we end
+    }
+}
+
+extension Double {
+    func truncate(places : Int) -> Double {
+        return Double(floor(pow(10.0, Double(places)) * self) / pow(10.0, Double(places)))
     }
 }
